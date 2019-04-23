c3dbc17891db8bf9e39cc52bbffff7352a8c9c40
dinhtai040197@gmail.com <dinhtai040197@gmail.com>
2019-04-23 11:33:10 +0300

    new

--- application/controllers/Delivery.php
+++ application/controllers/Delivery.php
@@ -6,8 +6,7 @@ class Delivery extends CI_Controller{
     parent::__construct();
     $this->load->model('Delivery_model');
   }
-  function show_delivery()
-  {
+  function show_delivery() {
     $data['customer']=$this->Delivery_model->getDelivery();
     $data['page']='delivery/show_delivery';
     $this->load->view('menu/content',$data);
@@ -22,6 +21,28 @@ class Delivery extends CI_Controller{
     $data['items'] = $this->Search_model->searching($keyword);
     $this->load->view('search/results_searching', $data);
   }
-
+  function add_form() {
+    $data['page']='search/add_form';
+    $this->load->view('menu/content', $data);
+  }
+  function add_customer() {
+    $insert_data=array(
+      "iddelivery" => $this->input->post('iddelivery'),
+      "idBills" => $this->input->post('idBills'),
+      "firstname" => $this->input->post('firstname'),
+      "lastname" => $this->input->post('lastname'),
+      "streetAdress " => $this->input->post('streetAdress'),
+      "date" => $this->input->post('date')
+    );
+    $result=$this->Delivery_model->add_customer($insert_data);
+    if ($result==1) {
+      $data['message']="You have added a new delivery";
+    }
+    else {
+      $data['message']="Something went wrong!";
+    }
+    $data['page']='search/add_info';
+    $this->load->view('menu/content',$data);
+  }
 
 }
--- application/models/Delivery_model.php
+++ application/models/Delivery_model.php
@@ -8,13 +8,16 @@ class Delivery_model extends CI_Model{
       return $query->result();
     }
 
-    public function getDelivery()
-    {
+    public function getDelivery() {
       $this->db->select('iddelivery,idBills,firstname,lastname,streetAdress');
       $this->db->from('delivery');
       $this->db->join('bills', 'delivery.idBills=bills.idBills','inner');
       $this->db->join('customer', 'bills.idCustomer=customer.idCustomer', 'inner');
       return $this->db->get()->result_array();
+    }
+    public function add_customer($insert_data)  {
+      $this->db->insert('dilivery',$insert_data);
+      return $this->db->affected_rows();
     }
 
 }
