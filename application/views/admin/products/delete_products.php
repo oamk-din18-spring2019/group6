b53a2a5282dd8fa8f9453e2d041adc03de448e24
PhPhat_Bin <phphat.phanhongphat@gmail.com>
2019-04-29 15:25:10 +0300

    update

--- application/views/admin/products/delete_products.php
+++ application/views/admin/products/delete_products.php
@@ -1,5 +1,5 @@
 
-
+<div style="margin-left: 260px">
 <h2>Delete Products</h2>
 <p>
   Do you want to delete the :" <?php echo $chosen_products[0]['productName']; ?> ": <br>
@@ -9,8 +9,10 @@
 	  <li>ID : <?php echo $chosen_products[0]['idproducts']; ?></li>
 	  <li>Name : <?php echo $chosen_products[0]['productName']; ?></li>
 	  <li>Description : <?php echo $chosen_products[0]['description']; ?></li>
-	  <li>Brand and Type : <?php echo $chosen_products[0]['brand']['type']; ?></li>
+	  <li>Image : <?php echo '<img style="width:100px" src="'.$chosen_products[0]['image'].'"/>'; ?> </li>
 	</h3>
 </ul>
 <a href="<?php echo site_url('products/delete_products/').$chosen_products[0]['idproducts']; ?>"><button>DELETE</button></a>
 <a href="<?php echo site_url('products/show_products'); ?>"><button>CANCEL</button></a>
+
+</div>
