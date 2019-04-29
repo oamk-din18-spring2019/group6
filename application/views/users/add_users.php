4d111fa4456918b4adce163e94192fd99c9e0e59
PhPhat_Bin <phphat.phanhongphat@gmail.com>
2019-04-29 15:13:54 +0300

    check

--- application/views/users/add_users.php
+++
@@ -1,8 +0,0 @@
-<div style="text-align: center">
-<h2>Congragulation to Register... !!!</h2>
-
-<?php echo $message; ?><br>
-<a href="<?php echo site_url('products/mainpage');?>">
-  <button type="button" class="btn btn-danger">BACK</button><hr>
-</a>
-</div>
--- application/views/users/loginOK.php
+++
@@ -1,9 +0,0 @@
-<h3></h3>
-
- <div style="text-align: center">
-<h2>Welcome <?php echo 'Welcome '.$_SESSION['username'];?> go to OUR PAGE... !!!</h2>
-<br>
-<a href="<?php echo site_url('products/mainpage');?>">
-  <button type="button" class="btn btn-danger">BACK</button><hr>
-</a>
-</div>
