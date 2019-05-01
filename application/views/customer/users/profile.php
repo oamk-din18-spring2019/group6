69676eb4edd3de57020345640bdfec0d7199ecbf
loliology9029 <hungpham161100@gmail.com>
2019-05-01 16:26:13 +0300

    new

--- application/views/customer/users/profile.php
+++ application/views/customer/users/profile.php
@@ -34,5 +34,5 @@ foreach ($profile as $row) {
 }
     ?>
 </table>
-<div style="position:absolute;margin-left:30px"><a href="<?php echo site_url('customer/User/show_edit');?>"><button>Edit Profile</button></a></div>
+<a href="<?php echo site_url('customer/User/show_edit');?>"><button>Edit Profile</button></a>
 </div>
