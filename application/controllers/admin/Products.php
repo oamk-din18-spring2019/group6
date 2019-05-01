d0552e32edcca33813b32b4163f616b2861b46c7
PhPhat_Bin <phphat.phanhongphat@gmail.com>
2019-05-01 13:10:59 +0300

    update and check to combine branches together

--- application/controllers/admin/Products.php
+++ application/controllers/admin/Products.php
@@ -6,46 +6,16 @@ class products extends CI_Controller{
   public function __construct()
   {
     parent::__construct();
-    $this->load->model('products_model');
-  }
-
-  //main page
-  function mainpage()
-  {
-    $data['products']=$this->products_model->getProducts();
-    $data['page']='products/mainpage.php';
-    $this->load->view('menu/body',$data);
-  }
-  //chosen products
-  function chosen_products()
-  {
-    $data['page']='products/chosen_products.php';
-    $this->load->view('menu/body',$data);
+    $this->load->model('admin/products_model');
   }
 
   //show products is here
   function show_products()
-  { 
-    //search product
-    $keysearch = $this->input->post('search');
-    $data['product'] = $this->products_model->search_products($keysearch);
-
-    //show product
-    $data['products'] = $this->products_model->getProducts();
-
-    $data['page']='products/show_products';
-    $this->load->view('menu/body',$data);
+  {
+    $data['products']=$this->products_model->getProducts();
+    $data['page']='admin/products/show_products';
+    $this->load->view('layout/layout',$data);
   }
-
-  //search products is here
-  // function search_products()
-  // {
-  //   $keysearch = $this->input->post('search');
-  //   $data['product'] = $this->products_model->search_products($keysearch);
-  //   $data['page']='products/search_products';
-  //   $this->load->view('menu/body',$data);
-  // }
- 
 
   //add products is here
   function add_products()
@@ -57,8 +27,7 @@ class products extends CI_Controller{
       "brand" => $this->input->post('brand'),
       "type" => $this->input->post('type'),
       "quantity" => $this->input->post('quantity'),
-      "price" => $this->input->post('price'),
-      "image" => $this->input->post('img')
+      "price" => $this->input->post('price')
     );
     $result=$this->products_model->add_products($insert_data);
     if ($result==1) 
@@ -69,21 +38,21 @@ class products extends CI_Controller{
     {
       $data['message']="Something went wrong!";
     }
-    $data['page']='products/add_products';
-    $this->load->view('menu/body',$data);
+    $data['page']='admin/products/add_products';
+    $this->load->view('layout/layout',$data);
   }
 
   //delete products is here
    function show_delete($id)
    {
     $data['chosen_products']=$this->products_model->get_chosen($id);
-    $data['page']='products/delete_products';
-    $this->load->view('menu/body',$data);
+    $data['page']='admin/products/delete_products';
+    $this->load->view('layout/layout',$data);
   }
   function delete_products($id)
   {
     $this->products_model->delete_products($id);
-    redirect('products/show_products');
+    redirect('admin/products/show_products');
   }
 
   //edit products is here
@@ -91,8 +60,8 @@ class products extends CI_Controller{
   {
     $data['products']=$this->products_model->get_chosen($id);
     $data['current_id']=$id;
-    $data['page']='products/edit_products';
-    $this->load->view('menu/body',$data);
+    $data['page']='admin/products/edit_products';
+    $this->load->view('layout/layout',$data);
   }
 
   function edit_products()
@@ -105,10 +74,9 @@ class products extends CI_Controller{
       "brand" => $this->input->post('brand'),
       "type" => $this->input->post('type'),
       "quantity" => $this->input->post('quantity'),
-      "price" => $this->input->post('price'),
-      "image" => $this->input->post('img')
+      "price" => $this->input->post('price')
     );
     $result=$this->products_model->edit_products($update_data,$id);
-    redirect('products/show_products');
+    redirect('admin/products/show_products');
   }
 }
